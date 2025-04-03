63c70eb3e94635a5371d58029ea006534de83f80
RHN3 <rhn.0033@gmail.com>
2025-04-03 14:34:22 +0100

    Created a react app from the vite template and added a header component just to start off the development.

---
+++ personal-website/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./header.jsx";
+
+function App() {
+  return (
+    <body>
+      <Header />
+      <p>Rohaan this is the start</p>
+    </body>
+  );
+}
+
+export default App;
---
+++ personal-website/src/header.jsx
@@ -0,0 +1,5 @@
+function Header() {
+  return <h1>Rohaan Mohammad</h1>;
+}
+
+export default Header;
