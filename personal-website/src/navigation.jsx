9cde07f4d08f4fb81daa09526430435c819b06f2
RHN3 <rhn.0033@gmail.com>
2025-04-13 20:28:12 +0100

    started on some styling and will continue soon

--- personal-website/src/navigation.jsx
+++ personal-website/src/navigation.jsx
@@ -1,23 +1,18 @@
 import { Link } from "react-router-dom";
+import "./navigationBar.css";
 
 export default function Navigation() {
   return (
     <nav>
       <ul>
         <li>
-          <Link to={"/"}>
-            <button>Home</button>
-          </Link>
+          <Link to={"/"}>Home</Link>
         </li>
         <li>
-          <Link to={"/About"}>
-            <button>About</button>
-          </Link>
+          <Link to={"/About"}>About</Link>
         </li>
         <li>
-          <Link to={"*"}>
-            <button>Contact</button>
-          </Link>
+          <Link to={"*"}>Contact</Link>
         </li>
       </ul>
     </nav>
